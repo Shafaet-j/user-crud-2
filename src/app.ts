f1de8229d91cb24fcad0b1b16a741b0456f83894
Shafaet Hossen <lbshaft01@gmail.com>
2024-01-11 00:27:38 +0600

    all done

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { UserRoutes } from "./app/modules/user/user.route";
 const app: Application = express();
-const port = 5000;
+const port = 3000;
 
 // parser
 app.use(express.json());
